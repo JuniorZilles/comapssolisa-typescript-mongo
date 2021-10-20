59de0825c5b2e710a8d87a23cb7a57342631a655
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-20 16:17:40 -0300

    fix: change initialization of database

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import router from './routes'
-import './infra/mongo/index'
-
+import MongoDatabase from './infra/mongo/index'
+MongoDatabase.connect()
 class App {
 
     public server
