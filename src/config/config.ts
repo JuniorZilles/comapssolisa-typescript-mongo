3ad68a0134255a252cbb8419146b7aa52a85930a
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-20 10:17:26 -0300

    feat(config): include

---
+++ src/config/config.ts
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
+
+export default {
+  database:{
+    host: process.env.MONGO_HOST,
+    username: process.env.MONGO_USER,
+    password: process.env.MONGO_PASSWORD,
+    collection: process.env.MONGO_COLLECTION
+  }
+    
+}
