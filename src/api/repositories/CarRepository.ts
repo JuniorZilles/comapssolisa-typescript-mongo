588bf8a79c254df09e762e3aa93ef6b48847578e
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 09:39:19 -0300

    feat(car): create validId, findById and delete functions

--- src/api/repositories/CarRepository.ts
+++ src/api/repositories/CarRepository.ts
@@ -1,4 +1,5 @@
-import CarModel, { Car }  from "@models/CarModel";
+import CarModel, { Car, isValid }  from "@models/CarModel";
+import { CarSearch } from "@models/CarSearchModel";
 import { VehiclesModel } from "@models/VehicleModel";
 
 class CarRepository  {
@@ -6,11 +7,23 @@ class CarRepository  {
       return await CarModel.create(payload)
     }
 
-    async findAll(payload:Object, start:number, size:number):Promise<VehiclesModel> {
+    async findAll(payload:CarSearch, start:number, size:number):Promise<VehiclesModel> {
       const count = await CarModel.countDocuments(payload)
       const cars = await CarModel.find(payload).skip( start ).limit( size ).exec()
       const offsets = Math.round(count/size)
       return new VehiclesModel(cars, count, size, start, offsets)
+    }
+
+    delete(id:string){
+
+    }
+
+    async findById(id:string):Promise<Car>{
+      return await CarModel.findById(id) as Car
+    } 
+
+    validId(id:string){
+      return isValid(id);
     }
   }
 
