f4cb01b76210fa93985ebed2f2dac6463e126745
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 09:35:28 -0300

    feat(error): included default not found error

---
+++ src/api/errors/NotFound.ts
@@ -0,0 +1,8 @@
+export class NotFound extends Error{
+    public status:number
+    constructor(name:String){
+        super(`Valor ${name} não encontrado`)
+        this.name = 'NotFound'
+        this.status = 404
+    }
+}
