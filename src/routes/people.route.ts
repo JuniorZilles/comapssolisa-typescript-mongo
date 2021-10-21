835b58de58cbbf90342f5ce1c554f3258aecbc20
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 19:47:37 -0300

    feat(people): set used validations

--- src/routes/people.route.ts
+++ src/routes/people.route.ts
@@ -1,13 +1,14 @@
 
 import PeopleController from "@controllers/PeopleController"
-import { Router } from "express";
+import { Router } from "express"
+import { CreateValidation, UpdateValidation, GetValidation } from "../../src/validations/peopleValidation"
 
 export default (prefix = '/people'): Router => {
   const router = Router()
-  router.post(`${prefix}/`, PeopleController.create)
-  router.get(`${prefix}/`, PeopleController.get)
+  router.post(`${prefix}/`, CreateValidation, PeopleController.create)
+  router.get(`${prefix}/`, GetValidation, PeopleController.get)
   router.get(`${prefix}/:id`, PeopleController.getById)
-  router.put(`${prefix}/:id`,PeopleController.update)
+  router.put(`${prefix}/:id`, UpdateValidation, PeopleController.update)
   router.delete(`${prefix}/:id`, PeopleController.delete)
   return router
 }
