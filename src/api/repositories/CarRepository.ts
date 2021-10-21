3f60dd78775b9d5290b683334981c520d9a4c044
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-20 21:17:34 -0300

    feat(car): insert find all function

--- src/api/repositories/CarRepository.ts
+++ src/api/repositories/CarRepository.ts
@@ -1,8 +1,16 @@
 import CarModel, { Car }  from "@models/CarModel";
+import { VehiclesModel } from "@models/VehicleModel";
 
 class CarRepository  {
     async create(payload:Car):Promise<Car> {
-      return await CarModel.create(payload);
+      return await CarModel.create(payload)
+    }
+
+    async findAll(payload:Object, start:number, size:number):Promise<VehiclesModel> {
+      const count = await CarModel.countDocuments(payload)
+      const cars = await CarModel.find(payload).skip( start ).limit( size ).exec()
+      const offsets = Math.round(count/size)
+      return new VehiclesModel(cars, count, size, start, offsets)
     }
   }
 
