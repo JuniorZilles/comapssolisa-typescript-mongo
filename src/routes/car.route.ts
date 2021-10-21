6f314cac6d50ba41ac0be31dd86759ed58eae036
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 17:39:51 -0300

    feat(route):include validation for get request

--- src/routes/car.route.ts
+++ src/routes/car.route.ts
@@ -1,12 +1,12 @@
 
 import CarController from "@controllers/CarController"
 import { Router } from "express";
-import { CreateValidation, UpdateValidation } from "../../src/validations/carValidation";
+import { CreateValidation, UpdateValidation, GetValidation } from "../../src/validations/carValidation";
 
 export default (prefix = '/car'): Router => {
     const router = Router()
     router.post(`${prefix}/`, CreateValidation, CarController.create)
-    router.get(`${prefix}/`, CarController.get)
+    router.get(`${prefix}/`, GetValidation, CarController.get)
     router.get(`${prefix}/:id`, CarController.getById)
     router.put(`${prefix}/:id`,UpdateValidation, CarController.update)
     router.delete(`${prefix}/:id`, CarController.delete)
