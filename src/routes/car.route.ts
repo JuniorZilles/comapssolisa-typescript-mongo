41ea7c227f89ef6b697119d9031d732e16096b23
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 11:01:53 -0300

    feat(car): insert all needed routes

--- src/routes/car.route.ts
+++ src/routes/car.route.ts
@@ -5,5 +5,9 @@ import { Router } from "express";
 export default (prefix = '/car'): Router => {
     const router = Router()
     router.post(`${prefix}/`, CarController.create)
+    router.get(`${prefix}/`, CarController.get)
+    router.get(`${prefix}/:id`, CarController.getById)
+    router.put(`${prefix}/:id`, CarController.update)
+    router.delete(`${prefix}/:id`, CarController.delete)
     return router
 }
