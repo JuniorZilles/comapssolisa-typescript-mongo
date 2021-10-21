30d3f805ece1b920bfcc90149a10b25625c7f3d1
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 17:40:51 -0300

    fix(error): returning 500 when should be 400

--- src/errors.ts
+++ src/errors.ts
@@ -1,10 +1,11 @@
 import { InvalidField } from "@errors/InvalidField"
+import { MissingBody } from "@errors/MissingBody"
 import { NotFound } from "@errors/NotFound"
 import { NextFunction, Request, Response } from "express"
 
 export default (error:any, req:Request, res:Response, next:NextFunction) =>{
     let status = 500
-    if (error instanceof NotFound || error instanceof InvalidField){
+    if (error instanceof NotFound || error instanceof InvalidField || error instanceof MissingBody){
         status = error.status
     }
     
