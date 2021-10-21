790c1ed81e50bbf21e5e19eabd41da9b901f6939
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 15:34:14 -0300

    feat(car): added validation for create and update cars

--- src/routes/car.route.ts
+++ src/routes/car.route.ts
@@ -1,13 +1,14 @@
 
 import CarController from "@controllers/CarController"
 import { Router } from "express";
+import { CreateValidation, UpdateValidation } from "../../src/validations/carValidation";
 
 export default (prefix = '/car'): Router => {
     const router = Router()
-    router.post(`${prefix}/`, CarController.create)
+    router.post(`${prefix}/`, CreateValidation, CarController.create)
     router.get(`${prefix}/`, CarController.get)
     router.get(`${prefix}/:id`, CarController.getById)
-    router.put(`${prefix}/:id`, CarController.update)
+    router.put(`${prefix}/:id`,UpdateValidation, CarController.update)
     router.delete(`${prefix}/:id`, CarController.delete)
     return router
 }
