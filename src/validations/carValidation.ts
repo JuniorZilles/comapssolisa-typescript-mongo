167934e1afea18eef87fd116b1f2b5aec9b5a811
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 15:35:14 -0300

    feat(car): included joi validation for creating and update

---
+++ src/validations/carValidation.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+
+import Joi from 'joi';
+
+export const CreateValidation = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const schema = Joi.object({
+            modelo: Joi.string().required(),
+            cor: Joi.string().required(),
+            ano: Joi.number().required(),
+            acessorios: Joi.array().items(
+                Joi.object({
+                    descricao: Joi.string().required()
+                })
+            ),
+            quantidadePassageiros: Joi.number().required()
+        });
+
+        const { error } = schema.validate(req.body, { abortEarly: true });
+        if (error) throw error
+        return next();
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+}
+
+export const UpdateValidation = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const schema = Joi.object({
+            modelo: Joi.string(),
+            cor: Joi.string(),
+            ano: Joi.number(),
+            acessorios: Joi.array().items(
+                Joi.object({
+                    descricao: Joi.string().required()
+                })
+            ).has(Joi.object({ descricao: Joi.string().valid('descricao')})),
+            quantidadePassageiros: Joi.number()
+        });
+
+        const { error } = schema.validate(req.params, { abortEarly: true });
+        if (error) throw error
+        return next();
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+}
