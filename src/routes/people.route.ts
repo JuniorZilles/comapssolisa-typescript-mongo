ca70ee0740590a8642f079a664fa1c5e71195aed
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 19:21:51 -0300

    feat(people): include all needed routes

--- src/routes/people.route.ts
+++ src/routes/people.route.ts
@@ -5,5 +5,9 @@ import { Router } from "express";
 export default (prefix = '/people'): Router => {
   const router = Router()
   router.post(`${prefix}/`, PeopleController.create)
+  router.get(`${prefix}/`, PeopleController.get)
+  router.get(`${prefix}/:id`, PeopleController.getById)
+  router.put(`${prefix}/:id`,PeopleController.update)
+  router.delete(`${prefix}/:id`, PeopleController.delete)
   return router
 }
