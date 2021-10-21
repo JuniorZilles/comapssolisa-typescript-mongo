07cda2ff5fca16357040ff72f6000859740b793b
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 19:46:23 -0300

    refactor(car): remove ';'

--- src/routes/car.route.ts
+++ src/routes/car.route.ts
@@ -1,14 +1,14 @@
 
 import CarController from "@controllers/CarController"
-import { Router } from "express";
-import { CreateValidation, UpdateValidation, GetValidation } from "../../src/validations/carValidation";
+import { Router } from "express"
+import { CreateValidation, UpdateValidation, GetValidation } from "../../src/validations/carValidation"
 
 export default (prefix = '/car'): Router => {
     const router = Router()
     router.post(`${prefix}/`, CreateValidation, CarController.create)
     router.get(`${prefix}/`, GetValidation, CarController.get)
     router.get(`${prefix}/:id`, CarController.getById)
-    router.put(`${prefix}/:id`,UpdateValidation, CarController.update)
+    router.put(`${prefix}/:id`, UpdateValidation, CarController.update)
     router.delete(`${prefix}/:id`, CarController.delete)
     return router
 }
