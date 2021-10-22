014b4d721c3dbf26d4a2f9ba8cfe04c09c97fc72
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 22:03:52 -0300

    feat(person): add adicional fields

--- src/api/models/PersonUpdateModel.ts
+++ src/api/models/PersonUpdateModel.ts
@@ -1,8 +1,10 @@
 export interface PersonUpdateModel {
+    id?: string
     nome?: string
     cpf?: string
     data_nascimento?: string
     email?: string
     senha?: string
     habilitado?: string
+    dataCriacao?: Date
 }
