9dbc4a374e1b3d1d0b68a462edc9ed107571ccc0
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 22:19:46 -0300

    feat(people): working list of people

--- src/api/services/PeopleService.ts
+++ src/api/services/PeopleService.ts
@@ -3,54 +3,56 @@ import { PersonCreateModel } from "@models/PersonCreateModel"
 import PeopleRepository from "@repositories/PeopleRepository"
 import { InvalidField } from '@errors/InvalidField'
 import { PersonUpdateModel } from '@models/PersonUpdateModel'
+import { PersonSearch } from '@models/PersonSearch'
 
 
-export class PeopleService{
-    async create(payload: PersonCreateModel):Promise<PersonCreateModel> {
+export class PeopleService {
+    async create(payload: PersonCreateModel): Promise<PersonCreateModel> {
         this.isOlder(payload.data_nascimento)
         return await PeopleRepository.create(payload)
     }
 
-    isOlder(date:string){
+    isOlder(date: string) {
         const birthday = moment(date, 'DD/MM/YYYY').format(
             'YYYY-MM-DD HH:mm:ss'
-          )
+        )
         const age = moment().diff(birthday, 'years', false)
-        if(age<18){
+        if (age < 18) {
             throw new InvalidField('data_nascimento')
         }
     }
 
-    async getById(id:string) { 
-        
+    async getById(id: string) {
+
     }
 
-    async list(payload:any) {
-        let start:number = 0
-        let size:number=10
-        
-        if(payload.size){
+    async list(payload: PersonSearch) {
+        let start: number = 0
+        let size: number = 10
+
+        if (payload.size) {
             size = payload.size
             payload.size = undefined
         }
-        if(payload.start){
+        if (payload.start) {
             start = payload.start
             payload.start = undefined
-        }  
+        }
+        return await PeopleRepository.findAll(payload, start, size)
     }
 
-    async delete(id:string) { 
+    async delete(id: string) {
         await this.getById(id)
-       
+
     }
 
-    async update(id:string, payload:PersonUpdateModel) { 
+    async update(id: string, payload: PersonUpdateModel) {
         await this.getById(id)
-        if(payload.data_nascimento){
+        if (payload.data_nascimento) {
             this.isOlder(payload.data_nascimento)
         }
-        
-        
+
+
     }
 }
 
