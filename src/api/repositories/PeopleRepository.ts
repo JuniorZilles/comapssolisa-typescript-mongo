89861ec653d812560f7d870cd8059d6307daa840
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-21 22:04:28 -0300

    feat(people): add all needed functions

--- src/api/repositories/PeopleRepository.ts
+++ src/api/repositories/PeopleRepository.ts
@@ -1,30 +1,37 @@
+import { PeopleModel } from "@models/PeopleModel";
 import { PersonCreateModel } from "@models/PersonCreateModel";
 import PersonModel, { isValid } from "@models/PersonModel";
+import { PersonSearch } from "@models/PersonSearch";
+import { PersonUpdateModel } from "@models/PersonUpdateModel";
 
 class CarRepository  {
     async create(payload:PersonCreateModel):Promise<PersonCreateModel> {
         return await PersonModel.create(payload)
     }
 
-    async findAll(payload:any, start:number, size:number):Promise<any> {
-     
+    async findAll(payload:PersonSearch, start:number, size:number):Promise<PeopleModel> {
+        const count = await PersonModel.countDocuments(payload)
+        const people = await PersonModel.find(payload).skip( start ).limit( size ).exec()
+        const offsets = Math.round(count/size)
+        return new PeopleModel(people, count, size, start, offsets)
     }
 
     async delete(id:string):Promise<boolean>{
-      
-      return true
+        await PersonModel.findByIdAndRemove(id).exec()
+        return true
     }
 
-    async findById(id:string):Promise<any>{
-     
+    async findById(id:string):Promise<PersonUpdateModel>{
+        return await PersonModel.findById(id) as PersonUpdateModel
     } 
 
     validId(id:string):boolean{
       return isValid(id);
     }
 
-    async update(id:string, payload:any){
-      
+    async update(id:string, payload:PersonUpdateModel){
+        await PersonModel.findByIdAndUpdate(id, payload).exec()
+        return true
     }
   }
 
