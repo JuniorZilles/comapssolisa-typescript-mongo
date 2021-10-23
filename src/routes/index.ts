0acf85a20d6a96372c650b2841d2dfc3e64375d6
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-23 09:00:28 -0300

    feat: added swagger to document the API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,13 @@ import { Express } from 'express'
 import routerPeople from './people.route'
 import routerCar from './car.route'
 import routerAuthenticate from './authenticate.route'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from '../static/swagger_layout.json'
 
 const V1Prefix = '/api/v1'
 
 export default (server:Express):void => {
+    server.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
     server.use(V1Prefix, routerPeople())
     server.use(V1Prefix, routerCar())
     server.use(V1Prefix, routerAuthenticate())
