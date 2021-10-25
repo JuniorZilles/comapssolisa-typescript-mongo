9c1d02d2e9e4bc5afb9a6aa3a6ca1e7955cc5cca
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-25 10:00:56 -0300

    fix: removed randexp

--- __tests__/utils/PeopleFactory.ts
+++ __tests__/utils/PeopleFactory.ts
@@ -1,11 +1,10 @@
 import faker from 'faker'
 import { factory } from 'factory-girl'
-import RandExp from 'randexp'
 import  PersonModel  from '@models/PersonModel'
 
 factory.define('People', PersonModel,{
     nome: faker.name.findName(),
-    cpf: new RandExp(/[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/),
+    cpf: '131.147.860-49',
     data_nascimento: faker.datatype.datetime(),
     email: faker.internet.email(),
     senha:  faker.internet.password(),
