017c1788c788aa729118f0c2ee7e875ad1317081
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-25 10:02:15 -0300

    feat(people): add some extra arguments to schema

--- src/api/models/PersonModel.ts
+++ src/api/models/PersonModel.ts
@@ -3,10 +3,10 @@ import {PersonCreateModel} from './PersonCreateModel'
 
 const PersonSchema = new mongoose.Schema({
     nome: {type: String, required:true },
-    cpf: {type: String, required:true },
+    cpf: {type: String, required:true},
     data_nascimento: {type: Date, required:true },
-    email: {type: String, required:true },
-    senha:  {type: String, required:true },
+    email: {type: String, required:true, unique: true, lowercase: true },
+    senha:  {type: String, required:true, select:false },
     habilitado:  {type: String, required:true,  enum: ['sim', 'não'] },
     dataCriacao: { type: Date, default: Date.now }
 })
