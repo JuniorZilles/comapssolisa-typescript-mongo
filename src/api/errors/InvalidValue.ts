5e33dd5dbe6c6fd66cf4b818d1a77dca8c5f8db8
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-26 09:52:02 -0300

    feat(error): insert invalid value default error

---
+++ src/api/errors/InvalidValue.ts
@@ -0,0 +1,10 @@
+export class InvalidValue extends Error{
+
+    public status:number
+    
+    constructor(field:string){
+        super(`O valor do campo '${field}' informado está inválido`)
+        this.name = 'InvalidField'
+        this.status = 400
+    }
+}
