ae83674902828d57ea4751bf69eabfb5b17d4791
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-26 09:53:34 -0300

    feat: include error on middleware of errors

--- src/errors.ts
+++ src/errors.ts
@@ -1,11 +1,12 @@
 import { InvalidField } from "@errors/InvalidField"
+import { InvalidValue } from "@errors/InvalidValue"
 import { MissingBody } from "@errors/MissingBody"
 import { NotFound } from "@errors/NotFound"
 import { NextFunction, Request, Response } from "express"
 
 export default (error:any, req:Request, res:Response, next:NextFunction) =>{
     let status = 500
-    if (error instanceof NotFound || error instanceof InvalidField || error instanceof MissingBody){
+    if (error instanceof NotFound || error instanceof InvalidField || error instanceof MissingBody || error instanceof InvalidValue){
         status = error.status
     }
     
