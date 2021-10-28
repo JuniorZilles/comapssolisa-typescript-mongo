3d1dd5f376c266db7c84630a672169ce57c87087
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-28 17:06:17 -0300

    feat: unique cpf and email

--- __tests__/utils/PeopleFactory.ts
+++ __tests__/utils/PeopleFactory.ts
@@ -4,9 +4,9 @@ import PersonModel from '@models/PersonModel';
 
 factory.define('People', PersonModel, {
   nome: faker.name.findName(),
-  cpf: `${faker.datatype.number({ max: 999, min: 100 })}.${faker.datatype.number({ max: 999, min: 100 })}.${faker.datatype.number({ max: 999, min: 100 })}-${faker.datatype.number({ max: 99, min: 10 })}`,
+  cpf: factory.sequence(() => `cpf${faker.datatype.number({ max: 999, min: 100 })}.${faker.datatype.number({ max: 999, min: 100 })}.${faker.datatype.number({ max: 999, min: 100 })}-${faker.datatype.number({ max: 99, min: 10 })}`),
   data_nascimento: faker.datatype.datetime(),
-  email: faker.internet.email(),
+  email: factory.sequence(() => `cpf${faker.internet.email()}`),
   senha: faker.internet.password(),
   habilitado: faker.random.arrayElement(['sim', 'não']),
 });
