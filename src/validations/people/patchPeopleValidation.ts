2f6b14f13228acd5b437f9744f96b987640307e0
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-28 09:46:57 -0300

    fix: entry date with format dd/mm/yyyy

--- src/validations/people/patchPeopleValidation.ts
+++ src/validations/people/patchPeopleValidation.ts
@@ -1,14 +1,17 @@
 /* eslint-disable no-useless-escape */
 import { NextFunction, Request, Response } from 'express';
 
+import Extension from '@joi/date';
 import Joi from 'joi';
+
+const JoiDate = Joi.extend(Extension);
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   try {
     const schema = Joi.object({
       nome: Joi.string(),
       cpf: Joi.string().regex(/[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/),
-      data_nascimento: Joi.date(),
+      data_nascimento: JoiDate.date().format('DD/MM/YYYY'),
       email: Joi.string().email(),
       senha: Joi.string().min(6),
       habilitado: Joi.string().valid('sim', 'não'),
--- src/validations/people/postPutPeopleValidation.ts
+++ src/validations/people/postPutPeopleValidation.ts
@@ -1,14 +1,17 @@
 /* eslint-disable no-useless-escape */
 import { NextFunction, Request, Response } from 'express';
 
+import Extension from '@joi/date';
 import Joi from 'joi';
+
+const JoiDate = Joi.extend(Extension);
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   try {
     const schema = Joi.object({
       nome: Joi.string().required(),
       cpf: Joi.string().regex(/[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/).required(),
-      data_nascimento: Joi.date().required(),
+      data_nascimento: JoiDate.date().format('DD/MM/YYYY').required(),
       email: Joi.string().email().required(),
       senha: Joi.string().min(6).required(),
       habilitado: Joi.string().valid('sim', 'não').required(),
