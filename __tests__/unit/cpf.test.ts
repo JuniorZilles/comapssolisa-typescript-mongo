e840c6101fe98254cad5a6adb727bad9df805d50
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-11-03 10:56:35 -0300

    feat: add cpf verification

---
+++ __tests__/unit/cpf.test.ts
@@ -0,0 +1,13 @@
+import validateCPF from '@services/CpfService';
+
+describe('src :: api :: services :: cpf', () => {
+  it('should return true for a valid cpf', async () => {
+    const isValid = validateCPF('967.056.960-52');
+    expect(isValid).toBe(true);
+  });
+
+  it('should not pass verification of invalid token', async () => {
+    const isValid = validateCPF('123.456.789-10');
+    expect(isValid).toBe(false);
+  });
+});
