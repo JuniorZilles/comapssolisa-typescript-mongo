b03218858092ff1216a7be5f0754a21308cdd5ca
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-11-16 10:19:08 -0300

    fix: removed cors

--- src/api/middlewares/headers.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { NextFunction, Request, Response } from 'express';
-
-export default (error: Error, req: Request, res: Response, next: NextFunction) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  next();
-};
