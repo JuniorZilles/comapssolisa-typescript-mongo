04e1a224455bdba3580aa7f0c8f86fe23d92688c
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-11-17 18:32:56 -0300

    feat: included fleet validation

---
+++ src/api/validations/rental/fleet/getRentalFleetValidation.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+
+import Joi from 'joi';
+import transformToArray from '@validations/utils/transformJoiResult';
+import { idRegex } from '@validations/utils/regex';
+
+export default async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
+  try {
+    const schema = Joi.object({
+      id_listagem: Joi.string().length(24).trim().regex(idRegex).message('Invalid id_listagem'),
+      id_locadora: Joi.string().length(24).trim().regex(idRegex).message('Invalid id_locadora'),
+      limit: Joi.number(),
+      offset: Joi.number()
+    });
+
+    const { error } = schema.validate(req.query, { abortEarly: false });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    return res.status(400).json(transformToArray(error as Joi.ValidationError));
+  }
+};
