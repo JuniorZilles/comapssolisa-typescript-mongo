761abfa90932616fd23dd046bbc6359d1203c08e
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-11-17 18:28:35 -0300

    feat: added validation of rental car route

---
+++ src/api/validations/rental/car/postputRentalCarValidation.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+
+import Joi from 'joi';
+import transformToArray from '@validations/utils/transformJoiResult';
+import { idRegex } from '@validations/utils/regex';
+
+export default async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
+  try {
+    const schema = Joi.object({
+      id_carro: Joi.string().length(24).trim().regex(idRegex).message('Invalid id_carro').required(),
+      status: Joi.string().trim().valid('disponível', 'indisponível').required(),
+      id_locacao: Joi.string().length(24).trim().regex(idRegex).message('Invalid id_locacao').required(),
+      valor_diaria: Joi.number().required(),
+      id_locadora: Joi.string().length(24).trim().regex(idRegex).message('Invalid id_locadora').required(),
+      placa: Joi.string().trim().required()
+    });
+
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    return res.status(400).json(transformToArray(error as Joi.ValidationError));
+  }
+};
